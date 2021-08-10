facc9239f591620f831326b95e196e025593436e
HarishRaj <harish.rajravichandran@rackspace.com>
2021-08-10 14:21:04 +0100

    pci dss control addition

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 resource "aws_securityhub_account" "security_hub" {}
 
+#pci dss standard control will not be enabled by default so separate source cretion is required
+resource "aws_securityhub_standards_subscription" "pci_321" {
+  depends_on    = [aws_securityhub_account.security_hub]
+  standards_arn = var.pci_standards_arn
+}
+
+#create members and send invite to member accounts
 resource "aws_securityhub_member" "security_hub_member" {
   depends_on = [aws_securityhub_account.security_hub]
   count      = length(var.account_ids)
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "account_ids" { type = list(string) }
 variable "email_ids" { type = list(string) }
 variable "enable_security_hub" { type = bool }
+
+variable "pci_standards_arn" {
+  type = string
+}
